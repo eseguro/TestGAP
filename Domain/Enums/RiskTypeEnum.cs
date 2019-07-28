b8015a33e734869e7e7bf9e4ca5761b287d1e77a
Estefanny Seguro <estefannyseguro.777@gmail.com>
2019-07-27 20:07:21 -0500

    Add dto's and coverage controller

---
+++ Domain/Enums/RiskTypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGAP.Domain.Enums
+{
+    public enum RiskTypeEnum
+    {
+        Low = 1,
+        Medium = 2,
+        MediumHigh = 3,
+        High = 4
+    }
+}
